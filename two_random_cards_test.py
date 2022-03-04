8109fd2a1be0dc4c4846b421150d65fe0609231a
Amanda Smith <amandasmith@Amandas-MacBook-Air.local>
2022-03-04 13:53:35 +0000

    creating more testss

--- two_random_cards_test.py
+++ two_random_cards_test.py
@@ -4,9 +4,28 @@ import unittest
 
 class TestCardGameApp(unittest.TestCase):
 
+    def setUp(self):
+        self.card_deck = two_random_cards.create_deck_of_cards()
+        self.shuffled_card_deck = two_random_cards.shuffle_deck(self.card_deck)
+
     def test_create_deck_of_cards(self):
-        card_deck_output = two_random_cards.create_deck_of_cards()
-        self.assertEqual(len(card_deck_output), 52, "Should be 52")
+        self.assertEqual(len(self.card_deck), 52, "Should be 52")
+
+    def test_create_deck_of_cards_count_jacks(self):
+        jacks = [card for card in self.card_deck if "Jack" in card]
+        self.assertEqual(len(jacks), 4, "Should be 4")
+
+    def test_create_deck_of_cards_count_twos(self):
+        jacks = [card for card in self.card_deck if "2" in card]
+        self.assertEqual(len(jacks), 4, "Should be 4")
+
+    def test_shuffle_deck(self):
+        print(self.shuffled_card_deck)
+        self.assertEqual(len(self.shuffled_card_deck), 52, "Should be 52")
+
+    def test_shuffle_deck_count_jacks(self):
+        jacks = [card for card in self.shuffled_card_deck if "Jack" in card]
+        self.assertEqual(len(jacks), 4, "Should be 4")
 
 
 if __name__ == '__main__':
