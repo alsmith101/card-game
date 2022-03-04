a128abd3ab308b0eef973295bf2778ec86a265a7
Amanda Smith <amandasmith@Amandas-MacBook-Air.local>
2022-03-04 14:01:56 +0000

    second set of tests passing

--- two_random_cards_test.py
+++ two_random_cards_test.py
@@ -2,7 +2,7 @@ import two_random_cards
 import unittest
 
 
-class TestCardGameApp(unittest.TestCase):
+class TestCardGameApp1(unittest.TestCase):
 
     def setUp(self):
         self.card_deck = two_random_cards.create_deck_of_cards()
@@ -10,21 +10,26 @@ class TestCardGameApp(unittest.TestCase):
     def test_create_deck_of_cards(self):
         self.assertEqual(len(self.card_deck), 52, "Should be 52")
 
+    # ensure that we get 4 jack cards one for each suit
     def test_create_deck_of_cards_count_jacks(self):
         jacks = [card for card in self.card_deck if "Jack" in card]
         self.assertEqual(len(jacks), 4, "Should be 4")
 
+    # ensure that we get 4 "two" cards one for each suit
     def test_create_deck_of_cards_count_twos(self):
         jacks = [card for card in self.card_deck if "2" in card]
         self.assertEqual(len(jacks), 4, "Should be 4")
 
-    # def test_shuffle_deck(self):
-    #     print(self.shuffled_card_deck)
-    #     self.assertEqual(len(self.shuffled_card_deck), 52, "Should be 52")
-    #
-    # def test_shuffle_deck_count_jacks(self):
-    #     jacks = [card for card in self.shuffled_card_deck if "Jack" in card]
-    #     self.assertEqual(len(jacks), 4, "Should be 4")
+
+class TestCardGameApp2(unittest.TestCase):
+
+    def setUp(self):
+        self.card_deck = [i for i in range(52)]
+        self.shuffled_card_deck = two_random_cards.shuffle_deck(self.card_deck)
+
+    # ensure that we get 52 cards back from the shuffling
+    def test_shuffle_deck(self):
+        self.assertEqual(len(self.shuffled_card_deck), 52, "Should be 52")
 
 
 if __name__ == '__main__':
